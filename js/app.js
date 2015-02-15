105d2b8fdea46e1eeddc02ede9ebf511d170b720
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2015-02-15 15:09:06 +0900

    Add perspective.

--- js/app.js
+++ js/app.js
@@ -1,56 +1,30 @@
+var mat = require('gl-mat4');
+
 var createProgram = require('./shader');
+var originalVertices = require('./vertices');
 
 module.exports = {
   init: init,
   draw: draw
 };
 
+var ROTATION_TIME = 5000;
+var EYE = [0, 0, -10];
+var VIEW_TRANSLATE = [0, 0, -7];
+
+var vertices = new Float32Array(originalVertices);
+
 var program;
 var buffer;
 
-var uniqueVertices = [
-  -1, -1, -1,
-  -1, -1, 1,
-  -1, 1, -1,
-  -1, 1, 1,
-  1, -1, -1,
-  1, -1, 1,
-  1, 1, -1,
-  1, 1, 1
-];
-
-var elements = [
-  0, 1,
-  1, 3,
-  3, 2,
-  2, 0,
-
-  4, 5,
-  5, 7,
-  7, 6,
-  6, 4,
-
-  0, 4,
-  1, 5,
-  2, 6,
-  3, 7
-];
-
-var originalVertices = elements.reduce(function(acc, index) {
-  acc.push(uniqueVertices[index * 3]);
-  acc.push(uniqueVertices[index * 3 + 1]);
-  acc.push(uniqueVertices[index * 3 + 2]);
-  return acc;
-}, []);
-
-var vertices = new Float32Array(originalVertices);
+var mvp = mat.create();
 
 function init(gl) {
   // Create shaders and program.
   var vertSrc = getScript('shader-vert');
   var fragSrc = getScript('shader-frag');
   var attributeNames = ['position'];
-  var uniformNames = ['transform'];
+  var uniformNames = ['mvp'];
   program = createProgram(gl, vertSrc, fragSrc, uniformNames, attributeNames);
 
   // Create buffer.
@@ -74,68 +48,30 @@ function draw(gl, t, volume) {
 
   gl.useProgram(program.program);
 
-  var ROTATION_TIME = 5000;
   var theta = (t % ROTATION_TIME) / ROTATION_TIME * Math.PI * 2;
-  var transform = multiply(multiply(rotateX(theta), rotateY(theta)), scale(0.3));
-  gl.uniformMatrix3fv(program.uniforms.transform, false, transform);
+  mat.identity(mvp);
 
+  // Perspective
+  mat.perspective(mvp, Math.PI / 4, w / h, 0.1, 100);
+
+  // Model View
+  mat.translate(mvp, mvp, VIEW_TRANSLATE);
+  mat.rotateX(mvp, mvp, theta);
+  mat.rotateY(mvp, mvp, theta);
+
+  gl.uniformMatrix4fv(program.uniforms.mvp, false, mvp);
+
+  // Draw cube lines 5 times.
   for (var i = 0; i < 5; i++) {
     randomVertices(volume * 0.03);
     gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-    gl.drawArrays(gl.LINES, 0, elements.length);
+    gl.drawArrays(gl.LINES, 0, vertices.length / 3);
   }
 }
 
 function getScript(id) {
   var script = document.getElementById(id);
   return script.innerText;
-}
-
-function rotateX(radian) {
-  var cos = Math.cos(radian);
-  var sin = Math.sin(radian);
-  return [
-    1, 0, 0,
-    0, cos, -sin,
-    0, sin, cos
-  ];
-}
-
-function rotateY(radian) {
-  var cos = Math.cos(radian);
-  var sin = Math.sin(radian);
-  return [
-    cos, 0, sin,
-    0, 1, 0,
-    -sin, 0, cos
-  ];
-}
-
-function multiply(mat1, mat2) {
-  var size = 3;
-  var result = new Array(size * size);
-  for (var i = 0; i < size * size; i++) {
-    result[i] = nth(i);
-  }
-  return result;
-
-  function nth(n) {
-    var x = n % size;
-    var y = (n - x) / size;
-    var sum = 0;
-    for (var i = 0; i < size; i++) {
-      sum += mat1[i + y * size] * mat2[x + i * size];
-    }
-    return sum;
-  }
-}
-
-function scale(num) {
-  return [
-    num, 0, 0,
-    0, num, 0,
-    0, 0, num
-  ];
 }
 
 function randomVertices(amplitude) {
